bcfceb355c548465c7d73fd50eb44511c1789c94
boop-101 <marc.flores075@students.cmcss.net>
2025-08-26 12:27:19 -0500

    Update game.ts

--- src/lib/constants/game.ts
+++ src/lib/constants/game.ts
@@ -24,7 +24,7 @@ export type RowCount = (typeof rowCountOptions)[number];
 /**
  * Interval (in milliseconds) for placing auto bets.
  */
-export const autoBetIntervalMs = 30;
+export const autoBetIntervalMs = 10;
 
 /**
  * For each row count, the background and shadow colors of each bin.
