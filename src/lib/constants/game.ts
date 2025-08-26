98f774efdb021deaa6ecd5b9f9c5e6cb98b65c00
boop-101 <marc.flores075@students.cmcss.net>
2025-08-26 13:18:19 -0500

    Update game.ts

--- src/lib/constants/game.ts
+++ src/lib/constants/game.ts
@@ -53,7 +53,7 @@ export const binProbabilitiesByRowCount: Record<RowCount, number[]> = rowCountOp
  */
 export const binPayouts: Record<RowCount, Record<RiskLevel, number[]>> = {
   8: {
-    [RiskLevel.LOW]: [.6, .5, .5, .5, 0.5, .5, .5, .5, .6],
+    [RiskLevel.LOW]: [.2, .2, .2, .2, 0.2, .2, .2, .2, .2],
     [RiskLevel.MEDIUM]: [13, 3, 1.3, 0.7, 0.4, 0.7, 1.3, 3, 13],
     [RiskLevel.HIGH]: [29, 4, 1.5, 1.2, 0.2, 0.3, 1.5, 4, 29],
   },
